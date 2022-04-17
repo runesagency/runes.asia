84ba550e0da33a5b5579450b5d66733b030433d3
Rafly Maulana <mraflymaulana@gmail.com>
2022-04-17 21:01:21 +0700

    use place-items to define item placing

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,6 @@
-export default function Heading({ title, description, className }: { title: string; description: string; className?: Record<"container" | "title" | "description", string> }) {
+export default function Heading({ title, description, className }: { title: string; description: string; className?: Partial<Record<"container" | "title" | "description", string>> }) {
     return (
-        <div className={`grid gap-2 mx-auto max-w-2xl text-center ${className?.container}`}>
+        <div className={`grid gap-2 mx-auto max-w-2xl text-center place-items-center ${className?.container}`}>
             <h1 className={`title text-linear-yellow bg-gradient-to-r max-w-full overflow-hidden break-words ${className?.title}`} data-typewriter>
                 {title}
             </h1>
