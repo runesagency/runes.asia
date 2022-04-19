72ce986a452f35ae36b3afa8479947add518c73d
Rafly Maulana <mraflymaulana@gmail.com>
2022-04-20 01:09:41 +0700

    Solved ref problem for Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,16 @@
-import type { AllHTMLAttributes } from "react";
+import type { ElementType, Ref } from "react";
 
-export default function Button({ className, ...props }: AllHTMLAttributes<HTMLAnchorElement>) {
-    return <a className={`from-yellow-light to-yellow-medium bg-gradient-to-r paragraph w-max font-bold px-5 py-3 hover:opacity-75 duration-200 ${className}`} {...props}></a>;
+import { forwardRef } from "react";
+
+interface CoreProps<T> {
+    as?: T | JSX.IntrinsicElements["a"];
 }
+
+// eslint-disable-next-line no-unused-vars
+const Button = <T extends keyof JSX.IntrinsicElements>({ className, ...props }: CoreProps<T> & JSX.IntrinsicElements[T], ref: Ref<any>): JSX.Element => {
+    const Component = (props.as || "a") as ElementType;
+
+    return <Component className={`from-yellow-light to-yellow-medium bg-gradient-to-r paragraph w-max font-bold px-5 py-3 hover:opacity-75 duration-200 ${className}`} {...props} />;
+};
+
+export default forwardRef(Button);
