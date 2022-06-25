594839d40db0a3affff641b026f2ef496a3adfc4
Rafly Maulana <mraflymaulana@gmail.com>
2022-06-26 03:33:11 +0700

    Add FAQ Page localization

---
+++ src/lib/localization/pages/faq.ts
@@ -0,0 +1,13 @@
+export const en = {
+    header: {
+        title: "Frequently Asked Question",
+        subtitle: "How Can We help you?",
+    },
+};
+
+export const id: typeof en = {
+    header: {
+        title: "Pertanyaan Yang Sering Diajukan",
+        subtitle: "Apa yang dapat kami bantu?",
+    },
+};
