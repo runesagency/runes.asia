a8b8a6d6b186367bced7df6ad7621bcb25a0953b
Rafly Maulana <mraflymaulana@gmail.com>
2022-06-26 01:55:29 +0700

    Fix props warning error in react

--- src/components/Images/Icons.tsx
+++ src/components/Images/Icons.tsx
@@ -54,26 +54,26 @@ export const Tiktok = (props: SVGAttributes<SVGElement>) => (
 );
 
 // Utility Icons
-export const Menu = (props: SVGAttributes<SVGElement> & { active?: boolean }) => (
-    <svg width="39" height={props.active ? "25" : "15"} viewBox={`0 0 39 ${props.active ? "25" : "15"}`} {...props}>
+export const Menu = ({ active, ...props }: SVGAttributes<SVGElement> & { active?: boolean }) => (
+    <svg width="39" height={active ? "25" : "15"} viewBox={`0 0 39 ${active ? "25" : "15"}`} {...props}>
         <rect
-            x={props.active ? "8.00006" : "1"}
-            y={props.active ? "22" : "0.5"}
-            width={props.active ? "30" : "39"}
+            x={active ? "8.00006" : "1"}
+            y={active ? "22" : "0.5"}
+            width={active ? "30" : "39"}
             height="3"
-            className={`duration-200 ease-in transform ${props.active && "-rotate-45"}`}
+            className={`duration-200 ease-in transform ${active && "-rotate-45"}`}
             style={{
-                transformOrigin: props.active && "8.00006px 22px",
+                transformOrigin: active && "8.00006px 22px",
             }}
         />
         <rect
-            x={props.active ? "9.99994" : "1"}
-            y={props.active ? "1" : "11.5"}
-            width={props.active ? "30" : "26"}
+            x={active ? "9.99994" : "1"}
+            y={active ? "1" : "11.5"}
+            width={active ? "30" : "26"}
             height="3"
-            className={`duration-200 ease-in transform ${props.active ? "rotate-45" : "group-hover:w-full"}`}
+            className={`duration-200 ease-in transform ${active ? "rotate-45" : "group-hover:w-full"}`}
             style={{
-                transformOrigin: props.active ? "9.99994px 1px" : "0px 11.5px",
+                transformOrigin: active ? "9.99994px 1px" : "0px 11.5px",
             }}
         />
     </svg>
