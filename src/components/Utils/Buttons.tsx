0baa4eafa1251cad3618e496fcc1ca98c8ff6e1e
Rafly Maulana <mraflymaulana@gmail.com>
2022-06-26 21:42:59 +0700

    Wrap component in forwardRef & change the element into an anchor

--- src/components/Utils/Buttons.tsx
+++ src/components/Utils/Buttons.tsx
@@ -1,20 +1,23 @@
-export const Primary = ({ className, light, ...props }: JSX.IntrinsicElements["button"] & { light?: boolean }) => (
-    <button
+/* eslint-disable react/display-name */
+/* eslint-disable no-unused-vars */
+import { forwardRef } from "react";
+
+export const Primary = forwardRef(({ className, light, ...props }: JSX.IntrinsicElements["a"] & { light?: boolean }, ref: any) => (
+    <a
         className={
             `font-poppins font-semibold w-max px-8 py-5 cursor-pointer hover:opacity-75 duration-200 rounded-2xl ${className} ` + //
             (light ? "bg-white text-black" : "bg-black text-white")
         }
         {...props}
     />
-);
+));
 
-// eslint-disable-next-line no-unused-vars
-export const Secondary = ({ className, light, active, ...props }: JSX.IntrinsicElements["button"] & { light?: boolean; active?: boolean }) => (
-    <button
+export const Secondary = forwardRef(({ className, light, active, ...props }: JSX.IntrinsicElements["a"] & { light?: boolean; active?: boolean }, ref: any) => (
+    <a
         className={
             `font-poppins w-max px-5 py-3 cursor-pointer hover:opacity-75 duration-200 rounded-full border-opacity-40 border ${className}` + //
             (active ? (light ? " bg-white text-black" : " bg-black text-white") : light ? " border-white text-white" : " border-black text-black")
         }
         {...props}
     />
-);
+));
