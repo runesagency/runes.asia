bbcd56d443a4ea8ff66d3685aecdcbbbdb39aaa8
Rafly Maulana <mraflymaulana@gmail.com>
2022-06-28 14:39:19 +0700

    Centering text in Button component

--- src/components/Utils/Buttons.tsx
+++ src/components/Utils/Buttons.tsx
@@ -5,7 +5,7 @@ import { forwardRef } from "react";
 export const Primary = forwardRef(({ className, light, ...props }: JSX.IntrinsicElements["a"] & { light?: boolean }, ref: any) => (
     <a
         className={
-            `font-poppins font-semibold w-max px-8 py-5 cursor-pointer hover:opacity-75 duration-200 rounded-2xl ${className} ` + //
+            `font-poppins font-semibold w-max px-8 py-5 cursor-pointer hover:opacity-75 duration-200 rounded-2xl text-center ${className} ` + //
             (light ? "bg-white text-black" : "bg-black text-white")
         }
         {...props}
@@ -15,7 +15,7 @@ export const Primary = forwardRef(({ className, light, ...props }: JSX.Intrinsic
 export const Secondary = forwardRef(({ className, light, active, ...props }: JSX.IntrinsicElements["a"] & { light?: boolean; active?: boolean }, ref: any) => (
     <a
         className={
-            `font-poppins w-max px-5 py-3 cursor-pointer hover:opacity-75 duration-200 rounded-full border-opacity-40 border ${className}` + //
+            `font-poppins w-max px-5 py-3 cursor-pointer hover:opacity-75 duration-200 rounded-full border-opacity-40 border text-center ${className}` + //
             (active ? (light ? " bg-white text-black" : " bg-black text-white") : light ? " border-white text-white" : " border-black text-black")
         }
         {...props}
