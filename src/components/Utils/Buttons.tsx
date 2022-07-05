f4a44f780e6383decb2c6abfae7ed97240d9d2bc
Rafly Maulana <mraflymaulana@gmail.com>
2022-07-05 14:29:16 +0700

    Button is now available as button or anchor

--- src/components/Utils/Buttons.tsx
+++ src/components/Utils/Buttons.tsx
@@ -1,23 +1,36 @@
 /* eslint-disable react/display-name */
 /* eslint-disable no-unused-vars */
-import { forwardRef } from "react";
+import { ElementType, forwardRef } from "react";
 
-export const Primary = forwardRef(({ className, light, ...props }: JSX.IntrinsicElements["a"] & { light?: boolean }, ref: any) => (
-    <a
-        className={
-            `font-poppins font-semibold w-max px-8 py-5 cursor-pointer hover:opacity-75 duration-200 rounded-2xl text-center ${className} ` + //
-            (light ? "bg-white text-black" : "bg-black text-white")
-        }
-        {...props}
-    />
-));
+type Button = JSX.IntrinsicElements["a" | "button"] & {
+    light?: boolean;
+    as?: "a" | "button";
+};
 
-export const Secondary = forwardRef(({ className, light, active, ...props }: JSX.IntrinsicElements["a"] & { light?: boolean; active?: boolean }, ref: any) => (
-    <a
-        className={
-            `font-poppins w-max px-5 py-3 cursor-pointer hover:opacity-75 duration-200 rounded-full border-opacity-40 border text-center ${className}` + //
-            (active ? (light ? " bg-white text-black" : " bg-black text-white") : light ? " border-white text-white" : " border-black text-black")
-        }
-        {...props}
-    />
-));
+export const Primary = forwardRef(({ className, light, ...props }: Button, ref: any) => {
+    const Component = (props.as || "a") as ElementType;
+
+    return (
+        <Component
+            className={
+                `font-poppins font-semibold w-max px-8 py-5 cursor-pointer hover:opacity-75 duration-200 rounded-2xl text-center ${className} ` + //
+                (light ? "bg-white text-black" : "bg-black text-white")
+            }
+            {...props}
+        />
+    );
+});
+
+export const Secondary = forwardRef(({ className, light, active, ...props }: Button & { active?: boolean }, ref: any) => {
+    const Component = (props.as || "a") as ElementType;
+
+    return (
+        <Component
+            className={
+                `font-poppins w-max px-5 py-3 cursor-pointer hover:opacity-75 duration-200 rounded-full border-opacity-40 border text-center ${className}` + //
+                (active ? (light ? " bg-white text-black" : " bg-black text-white") : light ? " border-white text-white" : " border-black text-black")
+            }
+            {...props}
+        />
+    );
+});
