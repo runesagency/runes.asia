a6955992c97ddd6b4833a4029ad874d9ffd8550f
Rafly Maulana <mraflymaulana@gmail.com>
2022-07-05 16:42:42 +0700

    Implement api in newsletter cta

--- src/components/Sections/NewsletterCTA.tsx
+++ src/components/Sections/NewsletterCTA.tsx
@@ -3,10 +3,46 @@ import * as Button from "@/components/Utils/Buttons";
 import * as Icon from "@/components/Images/Icons";
 
 import { useLanguage } from "@/lib/hooks";
+import { useState } from "react";
 import * as localization from "@/lib/localization/components/sections/newsletterCta";
 
 export default function NewsletterCTA() {
+    const [email, setEmail] = useState("");
     const { locale } = useLanguage("lang", localization);
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+
+        if (!email) return alert(locale.form.no_email);
+
+        fetch(`/api/cms/items/blog_mailing_list`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+            }),
+        }).then(async (res) => {
+            if (res.status === 200) {
+                setEmail("");
+                return alert(locale.form.success);
+            }
+
+            if (res.status === 400) {
+                const data = await res.json();
+
+                if (data.errors[0].extensions.code === "RECORD_NOT_UNIQUE") {
+                    setEmail("");
+                    return alert(locale.form.already_registered);
+                } else {
+                    return alert(locale.form.error);
+                }
+            }
+
+            return alert(locale.form.error);
+        });
+    };
 
     return (
         <CTA
@@ -23,13 +59,22 @@ export default function NewsletterCTA() {
                 <p className="subtitle text-justify">{locale.subtitle}</p>
             </div>
 
-            <form className="grid font-poppins max-w-xl">
+            <form className="grid font-poppins max-w-xl" onSubmit={handleSubmit}>
                 <div className="w-full flex gap-6 items-center border border-black border-opacity-20 pl-5 bg-white">
                     <Icon.Mail className="fill-current h-5" />
-                    <input type={"email"} placeholder="Your Email Account" className="py-4 w-full outline-none font-poppins" />
+                    <input
+                        type={"email"} //
+                        placeholder={locale.placeholder}
+                        className="py-4 w-full outline-none font-poppins"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        required
+                    />
                 </div>
 
-                <Button.Primary className="!w-full !rounded-none">{locale.button}</Button.Primary>
+                <Button.Primary as="button" className="!w-full !rounded-none" type="submit">
+                    {locale.button}
+                </Button.Primary>
             </form>
 
             <p className="font-poppins">{locale.notice}</p>
--- src/lib/localization/components/sections/newsletterCta.ts
+++ src/lib/localization/components/sections/newsletterCta.ts
@@ -2,12 +2,26 @@ export const en = {
     title: "Get The Latest Articles Every Week From Us.",
     subtitle: "Enhance your knowledge and explore the world through our writings.",
     button: "Join Mailing List",
+    placeholder: "Your Email Address",
     notice: "*Don’t worry, we won’t sent you any spam :)",
+    form: {
+        no_email: "Please enter your email address.",
+        success: "Successfully subscribed!",
+        already_registered: "Can't subscribe, you are already registered.",
+        error: "Something went wrong, please try again later.",
+    },
 };
 
 export const id: typeof en = {
     title: "Dapatkan Artikel Terbaru Setiap Minggunya.",
     subtitle: "Tingkatkan pengetahuan Anda dan jelajahi dunia melalui tulisan kami.",
     button: "Berlangganan",
-    notice: "*Tenang saja, kita tidak akan kirim spam ke anda :)",
+    placeholder: "Alamat Email Anda",
+    notice: "*Tenang saja, kami tidak akan kirim spam ke anda :)",
+    form: {
+        no_email: "Harap masukkan alamat email Anda.",
+        success: "Berhasil berlangganan!",
+        already_registered: "Tidak dapat berlangganan, Anda sudah terdaftar.",
+        error: "Terjadi kesalahan, silahkan coba lagi nanti.",
+    },
 };
