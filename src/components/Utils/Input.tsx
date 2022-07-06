7c9e1d6240686156fca942e4d161ff161dab5822
Rafly Maulana <mraflymaulana@gmail.com>
2022-07-06 11:41:06 +0700

    Make icon optional on input component

--- src/components/Utils/Input.tsx
+++ src/components/Utils/Input.tsx
@@ -1,11 +1,11 @@
 import * as Icon from "@/components/Images/Icons";
 
-export default function Input({ className, icon, ...props }: JSX.IntrinsicElements["input"] & { icon: keyof typeof Icon }) {
-    const InputIcon = Icon[icon];
+export default function Input({ className, icon, ...props }: JSX.IntrinsicElements["input"] & { icon?: keyof typeof Icon }) {
+    const InputIcon = icon ? Icon[icon] : null;
 
     return (
         <div className="w-full bg-white flex gap-6 items-center text-black border border-black border-opacity-20 pl-5">
-            <InputIcon className="fill-current h-5" />
+            {icon && <InputIcon className="fill-current h-5" />}
             <input className={`py-4 w-full outline-none font-poppins ${className}`} {...props} />
         </div>
     );
