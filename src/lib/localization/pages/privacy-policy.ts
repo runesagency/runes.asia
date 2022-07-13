7139fc7e63660db66adf3d0144d44fbfef4d0e98
Rafly Maulana <mraflymaulana@gmail.com>
2022-07-13 11:49:51 +0700

    add privacy policy page

---
+++ src/lib/localization/pages/privacy-policy.ts
@@ -0,0 +1,9 @@
+export const en = {
+    title: "Privacy Policy",
+    date: "Last Updated:",
+};
+
+export const id: typeof en = {
+    title: "Kebijakan Privasi",
+    date: "Terakhir diperbarui:",
+};
