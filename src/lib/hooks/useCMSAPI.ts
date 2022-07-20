aa1da7e1be2483355ae51285c03c4fdc15a12f78
Rafly Maulana <mraflymaulana@gmail.com>
2022-07-20 11:41:55 +0700

    Use type from fetchCMSAPI function

--- src/lib/hooks/useCMSAPI.ts
+++ src/lib/hooks/useCMSAPI.ts
@@ -1,34 +1,12 @@
-import type { RecursiveObject, Convert } from "@/lib/types";
+import type { Convert } from "@/lib/types";
+import type { fetchCMSAPIFields, fetchCMSAPIOptions } from "@/lib/functions/fetchCMSAPI";
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { fetchCMSAPI } from "@/lib/functions";
 
-type useCMSAPIFields = RecursiveObject<true | "*">;
-
-type useCMSAPIOptions<T> = {
-    // eslint-disable-next-line no-unused-vars
-    errorCallback?: (error: Error) => void;
-    defaultValue?: any;
-    fields?: T;
-    filter?: RecursiveObject<any>;
-    headers?: [string, string][] | { [key: string]: string };
-    search?: string;
-    sort?: string[];
-    deps?: any[];
-} & (
-    | {
-          limit: number;
-          page: number;
-      }
-    | {
-          skip: number; // offset
-          limit?: number;
-      }
-);
-
-export const useCMSAPI = <T extends useCMSAPIFields | [useCMSAPIFields]>(path: `/${string}`, options: useCMSAPIOptions<T>) => {
-    type Fields = T extends [useCMSAPIFields] ? [Convert<any, T[0]>] : Convert<any, T>;
+export const useCMSAPI = <T extends fetchCMSAPIFields | [fetchCMSAPIFields]>(path: `/${string}`, options: fetchCMSAPIOptions<T>) => {
+    type Fields = T extends [fetchCMSAPIFields] ? [Convert<any, T[0]>] : Convert<any, T>;
 
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState<Fields>(options?.defaultValue || null);
@@ -42,8 +20,8 @@ export const useCMSAPI = <T extends useCMSAPIFields | [useCMSAPIFields]>(path: `
                 setData(response);
                 setLoading(false);
             })
-            .catch((error) => {
-                alert(error);
+            .catch((error: Error) => {
+                alert(error.message);
                 router.push("/");
             });
 
