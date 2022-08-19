f7d59914d83e2c6056bb1b40c40297b6fcec2e5f
Rafly Maulana <mraflymaulana@gmail.com>
2022-08-19 17:52:56 +0700

    Replace page when language is changed
    - Blog translation is sent directly from server from getServerSideProps, it determines the language used with the slug used in the url
    - When the language is changed, it will replace the url with the one with slug translated on the desired language

--- src/pages/blog/[...slug].tsx
+++ src/pages/blog/[...slug].tsx
@@ -6,6 +6,7 @@ import NewsletterCTA from "@/components/Sections/NewsletterCTA";
 import * as Button from "@/components/Forms/Buttons";
 
 import moment from "moment";
+import { useRouter } from "next/router";
 import { encodeToURL, fetchCMSAPI } from "@/lib/functions";
 import { useLanguage } from "@/lib/hooks";
 import * as localization from "@/lib/localization/pages/blog/single";
@@ -20,6 +21,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
         defaultValue: {},
         deps: [id],
         fields: {
+            id: true,
             cover_image: true,
             user_created: "*",
             date_created: true,
@@ -39,7 +41,11 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 
     return {
         props: {
-            data,
+            data: {
+                ...data,
+                lang: translation.languages_code,
+                ...translation,
+            },
             seo: {
                 subtitle: translation?.title,
                 description: translation?.short_description,
@@ -50,11 +56,14 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 };
 
 export default function SingleBlogPage({ data }: { data: Awaited<ReturnType<typeof getServerSideProps>>["props"]["data"] }) {
-    const { lang, locale } = useLanguage("lang", localization);
+    const router = useRouter();
+    const { lang, locale, onLanguageChange } = useLanguage("lang", localization);
 
-    const blog = {
-        ...data,
-        ...(data.translations?.find((translation) => translation.languages_code === lang) || data.translations?.[0]),
+    onLanguageChange.current = (newLang) => {
+        if (newLang !== lang) {
+            const newBlog = data.translations?.find((translation) => translation.languages_code === newLang);
+            router.replace(`/blog/${data.id}/${encodeToURL(newBlog.title)}`);
+        }
     };
 
     return (
@@ -68,20 +77,20 @@ export default function SingleBlogPage({ data }: { data: Awaited<ReturnType<type
                         <div className="flex flex-col justify-between gap-24">
                             <div className="grid gap-7 text-white">
                                 <Button.Back text={locale.backButton} href="/blog" light />
-                                <h1 className="title">{blog.title}</h1>
-                                <h2 className="subtitle">{blog.short_description}</h2>
+                                <h1 className="title">{data.title}</h1>
+                                <h2 className="subtitle">{data.short_description}</h2>
                             </div>
 
                             <div className="flex flex-wrap items-center gap-3">
                                 <Button.Secondary as="button" active light className="bg-yellow-light">
-                                    {blog.user_created?.first_name} {blog.user_created?.last_name}
+                                    {data.user_created?.first_name} {data.user_created?.last_name}
                                 </Button.Secondary>
 
                                 <Button.Secondary as="button" light>
-                                    {moment(blog.date_created).format("MMMM DD, YYYY")}
+                                    {moment(data.date_created).format("MMMM DD, YYYY")}
                                 </Button.Secondary>
 
-                                {blog.tags?.map((tag: string, index: number) => (
+                                {data.tags?.map((tag: string, index: number) => (
                                     <Button.Secondary as="button" key={index} light className="capitalize">
                                         {tag.toLowerCase()}
                                     </Button.Secondary>
@@ -89,7 +98,7 @@ export default function SingleBlogPage({ data }: { data: Awaited<ReturnType<type
                             </div>
                         </div>
 
-                        <img src={`/api/cms/assets/${blog.cover_image}`} alt="" className="h-full w-full object-cover max-h-72 lg:max-h-full order-first lg:order-none" loading="lazy" />
+                        <img src={`/api/cms/assets/${data.cover_image}`} alt="" className="h-full w-full object-cover max-h-72 lg:max-h-full order-first lg:order-none" loading="lazy" />
                     </div>
                 </div>
             </section>
@@ -97,7 +106,7 @@ export default function SingleBlogPage({ data }: { data: Awaited<ReturnType<type
             {/* Content */}
             <section className="relative py-20">
                 <div className="container grid gap-6 prose lg:max-w-4xl font-poppins text-black">
-                    <div dangerouslySetInnerHTML={{ __html: blog.content }} />
+                    <div dangerouslySetInnerHTML={{ __html: data.content }} />
                     <Button.Back text={locale.backButton} href="/blog" className="mx-auto" />
                 </div>
             </section>
