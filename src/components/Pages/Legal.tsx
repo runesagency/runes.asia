578eb86bc92f0d4c96314e0586ee41d62f60aecf
Rafly Maulana <mraflymaulana@gmail.com>
2022-11-15 23:21:29 +0700

    Simplify legal page to same component and add skeleton loading

---
+++ src/components/Pages/Legal.tsx
@@ -0,0 +1,72 @@
+import Navigation from "@/components/Sections/Navigation";
+import Footer from "@/components/Sections/Footer";
+import LangChooser from "@/components/Utils/LangChooser";
+
+import moment from "moment";
+import { useCMSAPI, useLanguage } from "@/lib/hooks";
+
+export default function LegalPage({ endpoint, localization }: { endpoint: `/${string}`; localization: any }) {
+    const { locale, lang } = useLanguage("lang", localization);
+
+    const { data, loading } = useCMSAPI(endpoint, {
+        defaultValue: [],
+        skip: 0,
+        fields: {
+            translations: [
+                {
+                    languages_code: true,
+                    date_updated: true,
+                    content: true,
+                },
+            ],
+        },
+    });
+
+    const legal = data.translations?.find((item) => item.languages_code === lang);
+
+    return (
+        <main className="relative bg-white overflow-auto">
+            {/* Header */}
+            <section id="header" className="relative py-20">
+                <div className="container grid gap-28">
+                    <Navigation />
+
+                    <div className="grid gap-8 text-black mx-auto text-center">
+                        <LangChooser />
+
+                        <h1 className="title">{locale.title}</h1>
+
+                        <h2 className="subtitle">
+                            {locale.date} <b>{!loading && moment(legal?.date_updated).format("MMMM DD, YYYY, HH:MM:SS")}</b>
+                        </h2>
+                    </div>
+                </div>
+            </section>
+
+            {/* Content */}
+            <section id="content" className="relative pb-20">
+                <div className="container grid gap-28 font-poppins">
+                    {!loading ? ( //
+                        <article className="prose mx-auto lg:max-w-3xl" dangerouslySetInnerHTML={{ __html: legal?.content }} />
+                    ) : (
+                        <div className="grid gap-6 mx-auto lg:max-w-3xl w-full">
+                            {Array(8)
+                                .fill(0)
+                                .map((_, index) => (
+                                    <div
+                                        key={index}
+                                        className="h-4 bg-black bg-opacity-20 animate-pulse rounded-md"
+                                        style={{
+                                            width: `${Math.floor(Math.random() * (100 - 30 + 1) + 30)}%`,
+                                        }}
+                                    />
+                                ))}
+                        </div>
+                    )}
+                </div>
+            </section>
+
+            <Footer />
+        </main>
+    );
+}
--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,60 +1,7 @@
-import Navigation from "@/components/Sections/Navigation";
-import Footer from "@/components/Sections/Footer";
-import LangChooser from "@/components/Utils/LangChooser";
-import * as Icon from "@/components/Utils/Icons";
+import LegalPage from "@/components/Pages/Legal";
 
-import moment from "moment";
-import { useCMSAPI, useLanguage } from "@/lib/hooks";
 import * as localization from "@/lib/localization/pages/privacy-policy";
 
-export default function TermsOfServicePage() {
-    const { locale, lang } = useLanguage("lang", localization);
-
-    const { data, loading } = useCMSAPI("/items/privacy_policy", {
-        defaultValue: [],
-        skip: 0,
-        fields: {
-            translations: [
-                {
-                    languages_code: true,
-                    date_updated: true,
-                    content: true,
-                },
-            ],
-        },
-    });
-
-    const policy = data.translations?.find((item) => item.languages_code === lang);
-
-    return (
-        <main className="relative bg-white overflow-auto">
-            {/* Header */}
-            <section id="header" className="relative py-20">
-                <div className="container grid gap-28">
-                    <Navigation />
-
-                    <div className="grid gap-8 text-black mx-auto text-center">
-                        <LangChooser />
-                        <h1 className="title">{locale.title}</h1>
-                        <h2 className="subtitle">
-                            {locale.date} <b>{!loading && moment(policy?.date_updated).format("MMMM DD, YYYY, HH:MM:SS")}</b>
-                        </h2>
-                    </div>
-                </div>
-            </section>
-
-            {/* Content */}
-            <section id="content" className="relative pb-20">
-                <div className="container grid gap-28 font-poppins">
-                    {!loading ? ( //
-                        <article className="prose mx-auto lg:max-w-3xl" dangerouslySetInnerHTML={{ __html: policy?.content }} />
-                    ) : (
-                        <Icon.Loader className="h-32 mx-auto fill-black col-span-full" />
-                    )}
-                </div>
-            </section>
-
-            <Footer />
-        </main>
-    );
+export default function PrivacyPolicyPage() {
+    return <LegalPage endpoint="/items/privacy_policy" localization={localization} />;
 }
